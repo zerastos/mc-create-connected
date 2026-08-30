af314f8e8a6257324a45a2571e5d8967b7af3527
Henry Lin <henry.ys.lin@gmail.com>
2026-08-30 17:35:57 +0800

    Prevent stock duplication when connecting packagers to inventories via IAP/bridge (fixes #305)
    (cherry picked from commit 93504609f186462fc6c8dc91775fcadae003b8e4)
    
    # Conflicts:
    #	src/main/java/com/hlysine/create_connected/content/inventoryaccessport/InventoryAccessPortBlockEntity.java

--- src/main/java/com/hlysine/create_connected/CreateConnected.java
+++ src/main/java/com/hlysine/create_connected/CreateConnected.java
@@ -88,6 +88,7 @@ public class CreateConnected {
             CCMountedStorageTypes.register();
             CCDisplaySources.register();
             CCDisplayTargets.register();
+            CCInventoryIdentifiers.register();
         });
     }
 
